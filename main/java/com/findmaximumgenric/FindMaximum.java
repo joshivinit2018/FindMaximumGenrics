16f0b4456ac6bb6db3498704f078ae57909c9e39
joshivinit2018 <joshivinit2018@gmailcom>
2021-03-24 23:24:10 +0530

    [vinit] : add find max 3 string value

--- main/java/com/findmaximumgenric/FindMaximum.java
+++ main/java/com/findmaximumgenric/FindMaximum.java
@@ -29,4 +29,17 @@ public class FindMaximum {
         }
 
     }
+
+    public String findMaxString(String str1, String str2, String str3) {
+        String maxString =  str1;
+        if( str2.compareTo(maxString) > 0) {
+            maxString = str2;
+        }
+        if( str3.compareTo(maxString) > 0) {
+            maxString = str3;
+        }
+        return maxString;
+
+
+    }
 }
